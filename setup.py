b3591a5d94ab44b32fc0b3a9c47aceb1a82d8aca
TimSusa <timsusa@altus-insight.de>
2015-11-11 17:06:25 +0100

    minor

--- setup.py
+++ setup.py
@@ -9,17 +9,18 @@ setup(
     version='0.1',
     url='https://github.com/TimSusa/aptly_api_cli',
     license='MIT',
+    keywords = "aptly aptly-server debian",
     author='Tim Susa',
     author_email='timsusa@gmx.de',
     description='This python command line interface, executes calls to the Aptly server remotely, without blocking the Aptly database.',
     long_description=__doc__,
     packages=find_packages(),
-    py_modules=['aptly_cli', 'cli'],
-    entry_points={
-        'console_scripts': [
-            'aptly-cli=aptly_cli.cli:main'
-        ]
-    },
+    # py_modules=['aptly_cli', 'cli'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'aptly-cli=aptly_cli.cli:main'
+    #     ]
+    # },
     # data_files=[
     #     ('supervisor', ['contrib/rest-api.sv.conf']),
     # ],
